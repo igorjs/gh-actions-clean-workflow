226d7c868ce6bc7a23798c17266c7d7eedeaf424
Igor J. Santos <igor@igorjs.io>
2022-07-02 14:02:35 +1000

    fix: implements pagination over 100 workflow runs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { setFailed } from "@actions/core";
-import { getApiActions } from "./api.js";
 import { dateDiff, calcTimeUnits } from "./utils/date.js";
-import { getToken, getOwner, getRepo, getDaysOld } from "./helpers/params";
+import { getToken, getOwner, getRepo, getDaysOld } from "./helpers/params.js";
+import { getApi } from "./helpers/api.js";
 
 async function run() {
   try {
@@ -10,26 +10,24 @@ async function run() {
     const token = getToken("token");
     const numDaysOldToBeDeleted = getDaysOld("days_old");
 
-    const actions = getApiActions({ token, owner, repo });
-
-    const { data } = await actions.listWorkflowRunsForRepo();
+    const api = getApi({ token, owner, repo });
 
     const hasRunBeforeDate = (run) => {
       const diff = dateDiff(run.updated_at, Date.now());
       return calcTimeUnits(diff).days >= numDaysOldToBeDeleted;
     };
 
-    const workflowRunsToDelete = data.workflow_runs.filter(hasRunBeforeDate);
+    const workflowRuns = await api.listWorkflowRuns();
 
-    console.info(`${workflowRunsToDelete.length} workflow runs to be deleted`);
+    const workflowRunsToDelete = workflowRuns.filter(hasRunBeforeDate);
+
+    console.info("%d workflow runs to be deleted", workflowRunsToDelete.length);
 
     if (workflowRunsToDelete.length > 0) {
-      const results = await Promise.all(
-        workflowRunsToDelete.map(actions.deleteRunAction)
-      );
+      const results = await api.deleteRuns(workflowRunsToDelete);
 
       if (results.length > 0) {
-        console.info(`${results.length} workflow runs sucessfully deleted`);
+        console.info("%d workflow runs sucessfully deleted", results.length);
       } else {
         throw new Error(
           `The action could not delete any workflows. Please review your parameters.`
